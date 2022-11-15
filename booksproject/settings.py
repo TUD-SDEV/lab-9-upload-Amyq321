cc29675845470840b261f2a54d1a6feec71b7ec2
Amy Quirke <x00188837@mytudublin.ie>
2022-11-15 02:57:20 +0000

    lab9 part4

--- booksproject/settings.py
+++ booksproject/settings.py
@@ -143,6 +143,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 STATICFILES_FINDERS = [
-    "django.contrib.staticfiles.finders.FileSystemsFinder",
+    "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
